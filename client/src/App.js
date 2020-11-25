0ea5b111d363cb023ff2bfbf55d0492b5b3bb802
Hwanhee Chae <hh@hwan.me>
2020-11-25 16:04:53 +0900

    Add frontend UI add spotify copycat design add togglable play/stop button add togglable mute button add volume input for audio player

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import io from "socket.io-client";
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { faPlayCircle, faStopCircle, faVolumeDown, faVolumeMute } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 let SOCKET_URI = "/"
 if (process.env.NODE_ENV === "development") {
@@ -14,6 +16,8 @@ function App() {
   const [queue, setQueue] = useState([]);
   const [played, setPlayed] = useState([]);
   const [playing, setPlaying] = useState({});
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [muted, setMuted] = useState(false);
 
   useEffect(() => {
     socket.on('data', (msg) => {
@@ -23,23 +27,79 @@ function App() {
     });
   });
 
+  const playerRef = useRef();
+  const volumeRef = useRef();
+
   return (
     <div className="App">
-      <audio controls>
-        <source src="/stream" type="audio/mpeg"/>
-      </audio>
-      <h1>Current song is : {playing.title}</h1>
-      {queue.map(song => {
-        return (
-          <li>{song}</li>
-        )
-      })}
-      <hr/>
-      {played.map(song => {
-        return (
-          <li>{song}</li>
-        )
-      })}
+      <div className="container">
+        <h3 className="songListHeader">now playing</h3>
+        <li className="song playing">{playing.title}</li>
+        <h3 className="songListHeader">next in queue</h3>
+        <div className="queue">
+          {queue.map(song => {
+            return (
+              <li className="song">{song}</li>
+            )
+          })}
+        </div>
+        <h3 className="songListHeader">already played</h3>
+        <div className="played">
+          {played.map(song => {
+            return (
+              <li className="song">{song}</li>
+            )
+          })}
+        </div>
+      </div>
+      <footer>
+        <audio ref={playerRef}>
+          <source src="/stream" type="audio/mpeg"/>
+        </audio>
+        <button onClick={() => {
+          if (!isPlaying) {
+            playerRef.current.setAttribute("src", "/stream");
+            playerRef.current.play();
+            playerRef.current.volume = volumeRef.current.value;
+          } else {
+            playerRef.current.pause();
+            playerRef.current.setAttribute('src', "");
+          }
+          setIsPlaying(!isPlaying);
+        }}>
+          {!isPlaying ? (
+            <FontAwesomeIcon icon={faPlayCircle} size="2x"/>
+          ) : (
+            <FontAwesomeIcon icon={faStopCircle} size="2x"/>
+          )}
+        </button>
+        <div className="volumeControlWrap">
+          <button onClick={() => {
+            if (!muted) {
+              playerRef.current.volume = 0;
+            } else {
+              playerRef.current.volume = 0.05;
+            }
+            setMuted(!muted);
+          }}>
+            {!muted ? (
+              <FontAwesomeIcon icon={faVolumeDown} size="2x"/>
+            ) : (
+              <FontAwesomeIcon icon={faVolumeMute} size="2x"/>
+            )}
+          </button>
+          <div id="volumeControl">
+            <input type="range" min="0" max="0.2" step="0.01" defaultValue="0.05" ref={volumeRef} onChange={e => {
+              playerRef.current.volume = e.target.value;
+              if (playerRef.current.volume === 0) {
+                setMuted(true);
+              } else {
+                setMuted(false);
+              }
+            }}/>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
