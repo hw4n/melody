a6c9c3928bb20524eafe7b2bfedd322dcf351dca
Hwanhee Chae <hh@hwan.me>
2020-11-25 11:48:40 +0900

    Add socket.io client removed axios removed components and wrote them again in App.js with socket.io

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
-import Queue from './components/Queue';
-import Playing from './components/Playing';
+import io from "socket.io-client";
+import { useEffect, useState } from 'react';
+
+let SOCKET_URI = "/"
+if (process.env.NODE_ENV === "development") {
+  SOCKET_URI = "http://localhost:3333";
+}
+
+const socket = io.connect(SOCKET_URI);
 
 function App() {
+  const [queue, setQueue] = useState([]);
+  const [played, setPlayed] = useState([]);
+  const [playing, setPlaying] = useState({});
+
+  useEffect(() => {
+    socket.on('data', (msg) => {
+      setQueue(msg.queue);
+      setPlayed(msg.played);
+      setPlaying(msg.playing);
+    });
+  });
+
   return (
     <div className="App">
       <audio controls>
         <source src="/stream" type="audio/mpeg"/>
       </audio>
-      <Playing/>
-      <Queue/>
+      <h1>Current song is : {playing.title}</h1>
+      {queue.map(song => {
+        return (
+          <li>{song}</li>
+        )
+      })}
+      <hr/>
+      {played.map(song => {
+        return (
+          <li>{song}</li>
+        )
+      })}
     </div>
   );
 }
