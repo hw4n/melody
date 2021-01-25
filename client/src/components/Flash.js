7eec2cc14aed7a7fe310e3f68916ed5f665a0334
Hwanhee Chae <hh@hwan.me>
2021-01-25 19:19:25 +0900

    Remove default flash message removed because musics won't stop anymore

--- client/src/components/Flash.js
+++ client/src/components/Flash.js
@@ -4,9 +4,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import './Flash.css';
 
 function Flash() {
-  const [flashMessages, setFlashMessages] = useState([
-    "For Chrome - if audio stops when another window is in full screen, try disabling this flag: chrome://flags/#calculate-native-win-occlusion",
-  ]);
+  const [flashMessages, setFlashMessages] = useState([]);
 
   return (
     <div class="flashWrap">
