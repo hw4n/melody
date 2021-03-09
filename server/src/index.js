2baa67bd4fc7e7c2f9fecc512045b3e014085a64
Hwanhee Chae <hh@hwan.me>
2021-03-09 20:38:14 +0900

    Add express.json() middleware

--- server/src/index.js
+++ server/src/index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
