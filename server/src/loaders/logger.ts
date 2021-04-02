deb848f6196b33dd40e13b3984463a7a2080aba4
Hwanhee Chae <hh@hwan.me>
2021-04-02 14:15:38 +0900

    Make typescript (logger.js -> logger.ts)

--- server/src/loaders/logger.ts
+++ server/src/loaders/logger.ts
@@ -3,22 +3,22 @@ function timestamp() {
   return `[${d.toLocaleDateString()} ${d.toLocaleTimeString()}]`;
 }
 
-export function logRed(string) {
+export function logRed(string: string) {
   console.log('\x1b[31m%s\x1b[0m', `${timestamp()} ${string}`);
 }
 
-export function logGreen(string) {
+export function logGreen(string: string) {
   console.log('\x1b[32m%s\x1b[0m', `${timestamp()} ${string}`);
 }
 
-export function logYellow(string) {
+export function logYellow(string: string) {
   console.log('\x1b[33m%s\x1b[0m', `${timestamp()} ${string}`);
 }
 
-export function logCyan(string) {
+export function logCyan(string: string) {
   console.log('\x1b[36m%s\x1b[0m', `${timestamp()} ${string}`);
 }
 
-export function logWhite(string) {
+export function logWhite(string: string) {
   console.log('\x1b[37m%s\x1b[0m', `${timestamp()} ${string}`);
 }
