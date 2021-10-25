c142911e877e5936a3469abfae61b13875080c4b
Hwanhee Chae <hh@hwan.me>
2021-10-25 22:45:22 +0900

    feat: setup basic redux store

---
+++ client/src/redux/modules/socket.js
@@ -0,0 +1,50 @@
+// write about socket's action / reducer
+
+const SOCKET_CONNECT = "SOCKET/CONNECT";
+const SOCKET_INIT = "SOCKET/INIT";
+const SOCKET_PRIORITY = "SOCKET/PRIORITY";
+const SOCKET_PLAY_NEXT = "SOCKET/PLAY_NEXT";
+const SOCKET_TOTAL_USERS = "SOCKET/TOTAL_USERS";
+const SOCKET_RENEW_LYRICS = "SOCKET/RENEW_LYRICS";
+
+const INIT_STATE = {
+  socketId: "",
+  playing: {},
+  priority: [],
+  queue: [],
+  start: 0,
+  total_users: 1,
+}
+
+export default function reducer(state = INIT_STATE, action) {
+  switch (action.type) {
+    case SOCKET_CONNECT:
+      return {...state, socketId: action.socketId};
+    case SOCKET_INIT:
+      return Object.assign({}, action.data);
+    case SOCKET_PRIORITY:
+      // enqueue a music from queue array to priority array.
+      // music with id of action.musicId should be enqueued.
+      return state;
+    case SOCKET_PLAY_NEXT:
+      // push current playing music to queue array,
+      // dequeue a music from priority or queue array,
+      // set that music as current playing music.
+      // action.data: {
+      //   FROM_QUEUE: "queue"
+      //   id: "616977015d619401b017bae9"​
+      //   start: 1635168306577
+      // }
+      return state;
+    case SOCKET_TOTAL_USERS:
+      // set total users count
+      // action.total_users: Number
+      return state;
+    case SOCKET_RENEW_LYRICS:
+      // if lyric mode is on,
+      // set lyric mode off and on
+      return state;
+    default:
+      return state;
+  }
+}
