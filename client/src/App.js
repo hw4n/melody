d280c8b2b4661b011363b9647605d1be6313ec5f
Hwanhee Chae <hh@hwan.me>
2021-10-26 15:19:45 +0900

    refactor: separate code from App component to helper functions added setKeydownListeners(), setDocumentTitle(), setMediaSession()

--- client/src/App.js
+++ client/src/App.js
@@ -5,91 +5,24 @@ import Flash from "./components/Flash";
 import Lyrics from "./components/Lyrics";
 import EntirePlaylist from "./components/EntirePlaylist";
 import Footer from "./components/Footer";
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { setDocumentTitle, setKeydownListeners, setMediaSession } from './helper/app';
 
 const DEFAULT_TITLE = process.env.TITLE || "Melody";
 
 function App() {
-  const dispatch = useDispatch();
   const [loaderMounted, setLoaderMounted] = useState(true);
-  const { playing, queue, start } = useSelector(store => store.socket);
+  const { playing, queue } = useSelector(store => store.socket);
   const { isPlaying, isLyricMode } = useSelector(store => store.app);
 
   useEffect(() => {
-    document.addEventListener("keydown", (e) => {
-      // ignore keydown from inputs
-      if (e.target.tagName === "INPUT" ||
-          e.target.tagName === "TEXTAREA") {
-        return;
-      }
-      switch (e.key) {
-        case " ":
-          dispatch({type: "APP/TOGGLE_PLAYING"});
-          break;
-        case "m":
-        case "M":
-          dispatch({type: "APP/TOGGLE_MUTED"});
-          break;
-        case "l":
-        case "L":
-          dispatch({type: "APP/TOGGLE_LYRIC_MODE"});
-          break;
-        case "Escape":
-          dispatch({type: "APP/SET_LYRIC_MODE", setTo: false});
-          break;
-        default:
-          break;
-      }
-    });
+    setKeydownListeners(document);
   }, []);
 
+  // change document.title to current music title
   useEffect(() => {
-    if ('mediaSession' in navigator) {
-      const { title, artist, album } = playing
-
-      navigator.mediaSession.metadata = new window.MediaMetadata({
-        title: title,
-        artist: artist,
-        album: album,
-        artwork: [
-          { src: `/96.png?${start}`,  sizes: '96x96',   type: 'image/png' },
-          { src: `/128.png?${start}`, sizes: '128x128', type: 'image/png' },
-          { src: `/192.png?${start}`, sizes: '192x192', type: 'image/png' },
-          { src: `/256.png?${start}`, sizes: '256x256', type: 'image/png' },
-          { src: `/384.png?${start}`, sizes: '384x384', type: 'image/png' },
-          { src: `/512.png?${start}`, sizes: '512x512', type: 'image/png' },
-        ]
-      });
-
-      const actionHandlers = [
-        ['play',          () => {
-          dispatch({type: "APP/TOGGLE_PLAYING"});
-          navigator.mediaSession.playbackState = "playing";
-        }],
-        ['pause',         () => {
-          dispatch({type: "APP/TOGGLE_PLAYING"});
-          navigator.mediaSession.playbackState = "paused";
-        }],
-        ['previoustrack', () => { /* ... */ }],
-        ['nexttrack',     () => { /* ... */ }]
-      ];
-
-      for (const [action, handler] of actionHandlers) {
-        try {
-          navigator.mediaSession.setActionHandler(action, handler);
-        } catch (error) {
-          console.log(error);
-        }
-      }
-    }
-  }, [start]);
-
-  useEffect(() => {
-    if (isPlaying) {
-      document.title = `♪ Playing ${playing.title}`;
-    } else {
-      document.title = DEFAULT_TITLE;
-    }
+    setDocumentTitle(document, isPlaying ? `♪ Playing ${playing.title}` : DEFAULT_TITLE);
+    setMediaSession(window);
   }, [isPlaying, playing.title]);
 
   return (
--- client/src/helper/app.js
+++ client/src/helper/app.js
@@ -11,3 +11,77 @@ export function resetSearch() {
 export function setEditing(bool) {
   store.dispatch({type: "APP/SET_LYRIC_EDITING", setTo: bool});
 }
+
+export function setDocumentTitle(document, title) {
+  document.title = title;
+}
+
+export function setMediaSession(window) {
+  if ('mediaSession' in navigator) {
+    const { playing, start } = store.getState().socket;
+    const { title, artist, album } = playing;
+
+    navigator.mediaSession.metadata = new window.MediaMetadata({
+      title: title,
+      artist: artist,
+      album: album,
+      artwork: [
+        { src: `/96.png?${start}`,  sizes: '96x96',   type: 'image/png' },
+        { src: `/128.png?${start}`, sizes: '128x128', type: 'image/png' },
+        { src: `/192.png?${start}`, sizes: '192x192', type: 'image/png' },
+        { src: `/256.png?${start}`, sizes: '256x256', type: 'image/png' },
+        { src: `/384.png?${start}`, sizes: '384x384', type: 'image/png' },
+        { src: `/512.png?${start}`, sizes: '512x512', type: 'image/png' },
+      ]
+    });
+
+    const actionHandlers = [
+      ['play',          () => {
+        store.dispatch({type: "APP/TOGGLE_PLAYING"});
+        navigator.mediaSession.playbackState = "playing";
+      }],
+      ['pause',         () => {
+        store.dispatch({type: "APP/TOGGLE_PLAYING"});
+        navigator.mediaSession.playbackState = "paused";
+      }],
+      ['previoustrack', () => { /* ... */ }],
+      ['nexttrack',     () => { /* ... */ }]
+    ];
+
+    for (const [action, handler] of actionHandlers) {
+      try {
+        navigator.mediaSession.setActionHandler(action, handler);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+}
+
+export function setKeydownListeners(document) {
+  document.addEventListener("keydown", (e) => {
+    // ignore keydown from inputs
+    if (e.target.tagName === "INPUT" ||
+        e.target.tagName === "TEXTAREA") {
+      return;
+    }
+    switch (e.key) {
+      case " ":
+        store.dispatch({type: "APP/TOGGLE_PLAYING"});
+        break;
+      case "m":
+      case "M":
+        store.dispatch({type: "APP/TOGGLE_MUTED"});
+        break;
+      case "l":
+      case "L":
+        store.dispatch({type: "APP/TOGGLE_LYRIC_MODE"});
+        break;
+      case "Escape":
+        store.dispatch({type: "APP/SET_LYRIC_MODE", setTo: false});
+        break;
+      default:
+        break;
+    }
+  });
+}
