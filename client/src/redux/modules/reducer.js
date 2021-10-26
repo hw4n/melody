56c24450482c63a7316a2f87b4ff54cb7119fb28
Hwanhee Chae <hh@hwan.me>
2021-10-26 09:16:42 +0900

    feat: add reducer about overall app states

--- client/src/redux/modules/reducer.js
+++ client/src/redux/modules/reducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import socketReducer from './socket';
+import appReducer from './app';
 
 export default combineReducers({
   socketReducer,
+  app: appReducer
 });
