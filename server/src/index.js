ec0299b1a96da825769d946e4c53770791095042
Hwanhee Chae <hh@hwan.me>
2021-10-27 19:43:58 +0900

    fix: serving directory

--- server/src/index.js
+++ server/src/index.js
@@ -26,7 +26,7 @@ app.use(express.static('cover'));
 if (STAGE === 'live') {
   app.use(express.static('build'));
   app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+    res.sendFile(path.join(__dirname, '../build/index.html'));
   });
 }
 server.listen(PORT, () => {
