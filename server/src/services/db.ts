a451cd786180bbb736322b8207c3ddfb63a96772
Hwanhee Chae <hh@hwan.me>
2021-11-15 21:06:34 +0900

    refactor: sameMusicHasLyrics() logic

--- server/src/services/db.ts
+++ server/src/services/db.ts
@@ -3,18 +3,20 @@ import dbMusic, { IStoredMusic } from '../models/Music';
 // eslint-disable-next-line max-len
 export async function sameMusicHasLyrics(music: IStoredMusic): Promise<[boolean, { synced?: boolean | null, lyrics?: string | null}]> {
   const { duration, title, album } = music;
-  return dbMusic.findOne({
+  const foundMusic = await dbMusic.findOne({
     duration,
     title,
     album,
     lyrics: { $ne: '' || null },
-  }).then((foundMusic) => {
-    if (foundMusic === null || foundMusic.lyrics === undefined) {
-      return [false, {}];
-    }
-    const { synced, lyrics } = foundMusic;
-    return [foundMusic.lyrics.length > 0, { synced, lyrics }];
   });
+
+  // nothing found
+  if (foundMusic === null) {
+    return [false, {}];
+  }
+
+  const { synced, lyrics } = foundMusic;
+  return [foundMusic.lyrics.length > 0, { synced, lyrics }];
 }
 
 export function similarMusicHasLyrics() {
